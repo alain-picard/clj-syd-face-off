5bcee297662265a224eb1116c08a842ab5c5a123
Steve Buikhuizen <steve.buikhuizen@gmail.com>
2017-10-20 22:59:35 +1100

    reformatting pedestal files for Cursive, no other changes

--- src/the_great_face_off/server.clj
+++ src/the_great_face_off/server.clj
@@ -1,5 +1,5 @@
 (ns the-great-face-off.server
-  (:gen-class) ; for -main method in uberjar
+  (:gen-class)                                              ; for -main method in uberjar
   (:require [io.pedestal.http :as server]
             [io.pedestal.http.route :as route]
             [the-great-face-off.service :as service]))
@@ -12,17 +12,17 @@
   "The entry-point for 'lein run-dev'"
   [& args]
   (println "\nCreating your [DEV] server...")
-  (-> service/service ;; start with production configuration
-      (merge {:env :dev
+  (-> service/service                                       ;; start with production configuration
+      (merge {:env                     :dev
               ;; do not block thread that starts web server
-              ::server/join? false
+              ::server/join?           false
               ;; Routes can be a function that resolve routes,
               ;;  we can use this to set the routes to be reloadable
-              ::server/routes #(route/expand-routes (deref #'service/routes))
+              ::server/routes          #(route/expand-routes (deref #'service/routes))
               ;; all origins are allowed in dev mode
               ::server/allowed-origins {:creds true :allowed-origins (constantly true)}
               ;; Content Security Policy (CSP) is mostly turned off in dev mode
-              ::server/secure-headers {:content-security-policy-settings {:object-src "none"}}})
+              ::server/secure-headers  {:content-security-policy-settings {:object-src "none"}}})
       ;; Wire up interceptor chains
       server/default-interceptors
       server/dev-interceptors
